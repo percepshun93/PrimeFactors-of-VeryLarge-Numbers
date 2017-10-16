db272140b0db6a067046a29643cc7bb8445f7112
Subhasish Sarkar <subhasish.sarkar93@gmail.com>
2017-10-16 13:00:03 +0530

    Updated with better code
    There was an error in the logic where i was trying to find only the greatest prime number of a large number. That view would ignore the other large prime factors and would only show the greatest prime factor. I have included a list traversal which solves this. It is however not very efficient and is able to find prime factors of numbers with up to 14 digits in about 5 seconds. 
    I'll look into it and see if there is another more efficient way to do this.
    Thanks

--- primefac2.py
+++ primefac2.py
@@ -13,40 +13,54 @@ def isPrime(n):
 			return False
 			
 	return True
-	
+
+
 def highest_prime_factor(n):
-   if isPrime(n):
-      return n
-   for x in range(2, int(sqrt(n)+1)):
-      if not n % x:
-         return highest_prime_factor(n/x)
+	if isPrime(n):
+		return n
+	for x in range(2, int(sqrt(n)+1)):
+		if not n % x:
+			return highest_prime_factor(n/x)
 
 fact_list = []
 
 def isPrimeFact(n):
-
 	for i in islice(count(1), n):
-		if not n%i:
-			if isPrime(i):
+		if isPrime(i):
+			if not n%i:
 				fact_list.append(i)
 								
 
 num = int(input('Enter a number: '))
 
-if len(str(num)) >= 8:
+if len(str(num)) > 8:
 	x1 = int(highest_prime_factor(num))
 	#print('Highest Prime Factor of',num,'is:',x1)
-	fact_list.append(x1)	
+	fact_list.append(x1)
 	
-	if len(str(x1)) >= 8:
-			x2 = int(highest_prime_factor(x1-1))
-			fact_list.append(x2)
-			x3 = x2-1
-			isPrimeFact(x3)
-			fact_list.sort()
-			print('Prime factors of',num,'are:',fact_list)
-			
+	# for i in range(2,x1):
+		# isPrimeFact(num)
+		
+	if len(str(x1)) > 8:
+		for i in range(2,x1):
+			if isPrime(i):
+				if not num%i:
+					fact_list.append(i)
+					
+	else:
+		for i in range(2,x1):
+			if isPrime(i):
+				if not num%i:
+					fact_list.append(i)
+	
+	fact_list.sort()
+	elements = len(fact_list)
+	print('There are',elements,'Prime factors of',num)
+	print('Prime factors of',num,'are:',fact_list)
 
 else:
 	isPrimeFact(num)
+	fact_list.sort()
+	elements = len(fact_list)
+	print('There are',elements,'Prime factors of',num)
 	print('Prime factors of',num,'are:',fact_list)
